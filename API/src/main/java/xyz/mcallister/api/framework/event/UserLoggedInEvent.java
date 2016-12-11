86fb2c482ac34e23a63632daf846a1a5e16bcaf2
Seth McAllister <dozingducklet@Gmail.com>
2016-12-11 15:30:08 +0000

    Added UserLoggedInEvent

--- API/src/main/java/xyz/mcallister/api/framework/event/UserLoggedInEvent.java
+++ API/src/main/java/xyz/mcallister/api/framework/event/UserLoggedInEvent.java
@@ -1,8 +1,43 @@
 package xyz.mcallister.api.framework.event;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import xyz.mcallister.api.framework.user.User;
+
 /**
  * Created by sethm on 10/12/2016.
  */
-public class UserLoggedInEvent
+public class UserLoggedInEvent extends Event
 {
+    private static final HandlerList list = new HandlerList();
+    private final User user;
+    private final Player player;
+
+    public UserLoggedInEvent(User user, Player player)
+    {
+        this.user = user;
+        this.player = player;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    @Override
+    public HandlerList getHandlers()
+    {
+        return list;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+        return list;
+    }
 }
